4d6d2f7e531bc37f94e5cadce4910b486208004a
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-02-15 15:27:06 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['/src/styles.css'],
+
 })
+
+
 export class AppComponent {
   title = 'playedfrontend';
 }
+
